70c82bc906e76dd7f7bff9ca9aa4c4c8fe1810b2
LucaTomala <43435523+LucaTomala@users.noreply.github.com>
2018-12-18 20:12:47 +0100

    Update trim.cpp

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,41 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+ #include "trim.h"
+ #include <string.h>
+ void trim(const char* source, char* trimmed_string)
+ {
+   int counter = 0;
+   int firstLetter = 0;
+   int lastLetter = strlen(source) -1;
+
+   get_firstLetter(source, &firstLetter);
+   get_lastLetter(source, &lastLetter);
+
+   if (strlen(source) > 0 ) {
+     for (int i = firstLetter; i <= lastLetter; i++)
+     {
+       trimmed_string[counter] = source[i];
+       counter ++;
+     }
+   }
+   else
+   {
+     strcpy(trimmed_string, "");
+   }
+ }
+
+ void get_firstLetter(const char* source, int *firstLetter)
+ {
+   while (source[*firstLetter] == ' ') {
+     *firstLetter = *firstLetter+1;
+   }
+ }
+
+ void get_lastLetter(const char* source, int *lastLetter)
+ {
+   while (source[*lastLetter] == ' ') {
+     *lastLetter = *lastLetter-1;
+   }
+}
+
